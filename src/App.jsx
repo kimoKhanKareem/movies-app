7446698e580a47c205f1ddf3803502372f7a6273
Karim Adel <kimo.khan.karim@gmail.com>
2022-06-15 16:23:17 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
 import "./App.css";
+// import { Link } from 'react-router-dom';
 
 function App() {
   const [end, setEnd] = useState("");
@@ -18,34 +20,79 @@ function App() {
       .then(response => response.json())
       .then(response => setFilm(response.data.movies))
       .catch(err => console.error(err));
-  });
+  }, [end]);
+
   console.log("film", film)
 
-  const onSubmit =(e) =>{
+  const onSubmit = (e) => {
     e.preventDefault()
     setEnd(inputRef.current.value)
   }
 
   return (
-    <div className="App">
-        <form onSubmit={onSubmit}>
-          <input type={"text"} ref={inputRef} />
-          <button type ="submit">Submuit</button>
-        </form>
+    <StyledDiv className="App">
+      <form onSubmit={onSubmit}>
+        <input type={"text"} ref={inputRef}/>
+        <button type="submit">Submuit</button>
+      </form>
+      <div className={"column"}>
         {film.map((item, index) => {
           return (
-            <div key={index}>
-              <h1>
+            <div className={"rap"} key={index}>
+              <div className={"bordimage"}>
+              {/* <Link to="https://yts.mx/movies/sex-appeal-2022"> */}
+                <img src={item.medium_cover_image} style={{width: '250px'}} alt="..." />
+              {/* </Link> */}
+              </div>
+              <h3>
                 {item.title_long}
-              </h1>
-              <img src={item.medium_cover_image} alt="..." />
+              </h3>
+              <h4>
+                Rating
+                <span> {item.rating} </span>
+              </h4>
             </div>
           )
         })}
-
       </div>
-
+    </StyledDiv>
   )
 }
+//styled components
+const StyledDiv = styled.section`
+form{
+  margin-bottom: 5%;
+  input{
+    width: 100%;
+    justify-content: center;
+    height: 40px;
+  }
+  input{
+    font-size: 1.5rem;
+    color: red;
+  }
+  button{
+    margin-top: 1%;
+    width: 20%;
+    height: 40px;
+    font-size: 1.5rem;
+  }
+}
+width: 100%;
+padding: 5% 5%;
+text-align: center;
+.column{
+  display: flex;
+  justify-content: center;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 30px;
+.bordimage{
+  width: 250px;
+  height: 350px;
+  overflow: hidden;
+}
+}
+`
 
 export default App;
---
+++ src/Components/det.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const det = () => {
+  return (
+    <div className={"column"}>
+    {film.map((item, index) => {
+      return (
+        <div className={"rap"} key={index}>
+          <div className={"bordimage"}>
+          <Link to="https://yts.mx/movies/sex-appeal-2022">
+            <img src={item.medium_cover_image} style={{width: '250px'}} alt="..." />
+          </Link>
+          </div>
+          <h3>
+            {item.title_long}
+          </h3>
+          <h4>
+            Rating
+            <span> {item.rating} </span>
+          </h4>
+        </div>
+      )
+    })}
+  </div>
+
+  )
+}
+
+export default det
